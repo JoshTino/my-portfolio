437507b12002875d4002585dd184c8222cdc7ceb
Dangana Joshua <joshuatinoo33@gmail.com>
2026-02-01 00:30:57 +0100

    Updated hero section resized image and text

--- components/hero.jsx
+++ components/hero.jsx
@@ -7,9 +7,9 @@ const Hero = () => {
 			<div className="flex flex-col-reverse items-center md:flex-row md:justify-around md:w-11/12 gap-y-4 gap-x-7 m-auto p-4 my-7 lg:my-10">
 				
 				<div className="">
-					<h2 className="text-6xl w-fit font-inria font-bold italic md:text-5xl lg:text-6xl text-gray-800 mb-7">Hi there,</h2>
+					<h2 className="text-5xl w-fit font-inria font-bold italic md:text-5xl lg:text-6xl text-gray-800 mb-7">Hi there,</h2>
 					<h2 className="text-3xl font-inria font-bold italic md:text-4xl lg:text-5xl  bg-lightred rounded-r-3xl w-fit p-2 pr-5 mb-5">I am Dangana Joshua</h2>
-					<h2 className="text-2xl font-inria font-bold italic md:text-2xl text-gray-800 bg-[#D9D9D9] rounded-r-3xl w-fit p-2 pr-5 mb-5">Full-Stack Web Developer</h2>
+					<h2 className="text-xl font-inria font-bold italic md:text-2xl text-gray-800 bg-[#D9D9D9] rounded-r-3xl w-fit p-2 pr-5 mb-5">Full-Stack Web Developer</h2>
 					<p className="text-lg font-inria leading-11 italic md:text-2xl text-gray-800 mb-7 lg:text-3xl">I build fast modern and scalable web applications usings Laravel, Nodejs, and React.</p>
 					<div className="flex mb-17 justify-center md:justify-start">
 						<a href="#projects" className="font-inria text-white px-5 py-3 text-xl md:text-lg py-2 rounded-2xl bg-[#282828] mr-7">View my work</a>
@@ -47,7 +47,7 @@ const Hero = () => {
 
 				</div>
 				<div className="lg:w-10/12 flex flex-col">
-					<div className="size-87 md:size-75 lg:size-130 self-end">
+					<div className="size-80 md:size-75 lg:size-130 self-end">
 						<img src="/images/hero-image.jpg" className=" w-full h-full rounded-full object-cover"/>
 					</div>
 				</div>
